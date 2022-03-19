d84439fa93ba6b3e358adb5c079a05a683a6a2b9
fabiopanduri <fabio.panduri@hotmail.com>
2022-03-19 11:46:15 +0100

    neural_network.py: commented the __init__ function of the NeuralNetworks class

--- src/neural_network.py
+++ src/neural_network.py
@@ -1,14 +1,34 @@
 import numpy as np
 
-from typing import List
+from typing import List, Callable
 
 
 
 class NeuralNetwork:
-    def __init__(self, dimensions: List[int], weights: List['numpy_array'] = [], biases: List['numpy_array'] = []) -> None:
+    '''
+    This class includes all functionalities for a neural network.
+    '''
+
+    def __init__(self, dimensions: List[int], weights: List['numpy_array'] = [], biases: List['numpy_array'] = [], activation_functions: List[Callable] = []) -> None:
+
+        # list of dimensions of the neural network
         self.dimensions = dimensions
+
+        # number of layers the neural network has
+        self.layers: int = len(self.dimensions)
+
+        # list of all the weight matrices for the neural net
+        # entry at i is w^{i + 1}
         self.weights = weights
+
+        # list of all the bias vectors for the neural net
+        # entry at i is b^{i + 1}
         self.biases = biases
+
+        # list containing all the activation functions for each layer
+        # entry at i is \sigma^{i + 1}
+        self.activation_functions = activation_functions
+
 
     def initialize_network(self) -> None:
         '''
@@ -16,22 +36,21 @@ class NeuralNetwork:
         '''
 
         # initialize the weights matrices w^{l} with dimensions dim(l) x dim(l - 1)
-        for i, dim in enumerate(self.dimensions[1:], 1):
-            self.weights.append(np.random.rand(dim, self.dimensions[i - 1]))
+        for l, dim in enumerate(self.dimensions[1:], 1):
+            self.weights.append(np.random.rand(dim, self.dimensions[l - 1]))
 
         # initialite the bias vectors b^{l} with dimensions dim(l)
-        for i in self.dimensions[1:]:
-            self.biases.append(np.random.rand(i))
+        for l in self.dimensions[1:]:
+            self.biases.append(np.random.rand(l))
 
 
     def feed_forward(self, input_vector: 'numpy_array') -> 'numpy_array':
         '''
         This method feeds the input_vector through the network.
         '''
-
-
-
+   
         pass
+
 
     def backpropagation(self, cost: float) -> None:
         '''
