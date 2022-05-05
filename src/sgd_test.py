6059d88ddb3133550cda19ef21299aac0271fbd8
Fabio Panduri <fabio@fabio.home>
2022-05-05 23:42:33 +0200

    sgd_test.py: changed to use the same batch for training in each epoch. also found a structure that enables the NN to approximate a cosine function.

--- src/sgd_test.py
+++ src/sgd_test.py
@@ -1,69 +1,89 @@
 from neural_network import *
 import matplotlib.pyplot as plt
 import time
+import sys
 
-def function(x, a, b, c) -> float:
-    return a * x ** 2 + b * x + c
-
-def function_2(a, b, c) -> float: 
-    return a + b + c
+def f(x) -> float:
+	return (np.cos(x) + 1) / 2
 
 def main():
-    NN = NeuralNetwork([1, 8, 8, 8, 1], 0.1,
-            activation_functions = [np.vectorize(sigmoid) for _ in range(5)],
-            activation_functions_derivatives = [np.vectorize(sigmoid_derivative) for _ in range(5)]
-            )
-    NN.initialize_network()
+	NN = NeuralNetwork([1, 4, 1], 0.1,
+			activation_functions = [np.vectorize(ReLU) for _ in range(2)] + [np.vectorize(sigmoid) for _ in range(1)],
+			activation_functions_derivatives = [np.vectorize(ReLU_derivative) for _ in range(2)] +
+			[np.vectorize(sigmoid_derivative) for _ in range(1)]
+			)
+	NN.initialize_network()
 
-    error = []
-    for i in range(1000):
-        train = []
-        for _ in range(10000):
-            #a, b, c = np.random.uniform(), np.random.rand(), np.random.rand()
-            x = np.random.uniform(0, 2*np.pi)
-            train.append((np.array([x]), np.array([np.cos(x)])))
+	START = 0
+	STOP = np.pi 
 
-        time.sleep(0.5)
+	try:
+		epochs = int(sys.argv[1])
+	except IndexError:
+		epochs = 100
 
-        NN.stochastic_gradient_descent(train)
-        
-    #print(NN.feed_forward(train[0][0]), train[0])
+	try:
+		batch_size = int(sys.argv[2])
+	except IndexError:
+		batch_size = 1000
 
-    #print(NN.weights)
-    #print(NN.weights)
-        #print(NN.weights)
 
-        test = []
-        for _ in range(100):
-            #a, b, c = np.random.uniform(), np.random.rand(), np.random.rand()
-            x = np.random.uniform(0, 2*np.pi)
-            test.append((np.array([x]), np.array([np.cos(x)])))
+	train = []
+	for _ in range(batch_size):
+		#a, b, c = np.random.uniform(), np.random.rand(), np.random.rand()
+		x = np.random.uniform(START, STOP)
+		train.append((np.array([x]), np.array([f(x)])))
 
-        sum = 0
-        for t in test:
-            prediction = NN.feed_forward(t[0])
-            sum += abs(prediction - t[1])
+	#plt.figure()
+	#plt.plot([i[0] for i in train], [i[1] for i in train], ',')
 
-        #print(sum / len(test))
-        print(i)
-        error.append(sum / len(test))
+	error = []
+	print('')
+	for i in range(epochs):
+		if i % 100 == 0: print(f'{i}/{epochs}', end='\r')
 
-    print(f'{NN.weights=}')
-    print(f'{NN.biases=}')
+		NN.stochastic_gradient_descent(train)
+		
+	#print(NN.feed_forward(train[0][0]), train[0])
 
-    predict = []
-    for x in np.arange(0, 2*np.pi, 0.001):
-        predict.append(NN.feed_forward(np.array([x])))
+	#print(NN.weights)
+	#print(NN.weights)
+		#print(NN.weights)
 
-    actual = [np.cos(x) for x in np.arange(0, 2*np.pi, 0.001)]
+		test = []
+		for _ in range(10):
+			x = np.random.uniform(START, STOP)
+			test.append((np.array([x]), np.array([f(x)])))
 
-    #print(predict)
+		sum = 0
+		for t in test:
+			prediction = NN.feed_forward(t[0])
+			 #print(prediction)
+			sum += 0.5 * (prediction - t[1])**2
 
-    plt.figure()
-    plt.plot(np.arange(0, 2*np.pi, 0.001), predict)
-    plt.plot(np.arange(0, 2*np.pi, 0.001), actual)
-    plt.figure()
-    plt.plot(list(range(0, len(error))), error)
-    plt.show()
+		#print(sum)
+
+		#print(sum / len(test))
+		#print(i)
+		error.append(sum / len(test))
+
+	print(f'{NN.weights=}')
+	print(f'{NN.biases=}')
+
+	predict = []
+	for x in np.arange(START, STOP, 0.01):
+		print(x, NN.feed_forward(np.array([x])))
+		predict.append(NN.feed_forward(np.array([x])))
+
+	actual = [f(x) for x in np.arange(START, STOP, 0.01)]
+
+	#print(predict)
+
+	plt.figure()
+	plt.plot(np.arange(START, STOP, 0.01), predict)
+	plt.plot(np.arange(START, STOP, 0.01), actual)
+	plt.figure()
+	plt.plot(list(range(0, len(error))), error)
+	plt.show()
 
 if __name__ == '__main__': main()
