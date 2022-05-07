45b0de5436335f12b28d2819440136136e74bef6
Fabio Panduri <fabio.panduri@hotmail.com>
2022-05-07 14:24:31 +0200

    Added copyright notice to files of dql folder

--- src/deep_q_learning.py
+++ src/deep_q_learning.py
@@ -1,3 +1,10 @@
+# Copyright (C) 2022 Luis Hartmann and Fabio Panduri
+
+# This file is part of maturaarbeit_code.
+# maturaarbeit_code is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
+# maturaarbeit_code is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with maturaarbeit_code. If not, see <https://www.gnu.org/licenses/>.
+
 from neural_network import NeuralNetwork
 
 from typing import Tuple
