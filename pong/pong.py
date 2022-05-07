01988e515168ad81bef672b627e4ff9e35a58eb5
Fabio Panduri <fabio.panduri@hotmail.com>
2022-05-07 14:25:56 +0200

    Updated copyright notice

--- pong/pong.py
+++ pong/pong.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2022 Luis Hartmann
+# Copyright (C) 2022 Luis Hartmann and Fabio Panduri
 
 # This file is part of maturaarbeit_code.
 # maturaarbeit_code is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
