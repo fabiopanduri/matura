e76e31c4f658ef3733dd13e777553522a833e0b7
Fabio Panduri <fabio.panduri@hotmail.com>
2022-05-11 19:12:48 +0200

    neat.py: added comments and tested a more complex structure

--- NEAT/neat.py
+++ NEAT/neat.py
@@ -10,9 +10,13 @@ from typing import List
 
 class NodeGene:
 	def __init__(self, node_id, node_type, activation_function):
+		# id of the node 
 		self.node_id = node_id
+
+		# specifies the type of the function, either 'input', 'hidden' or 'output'
 		self.node_type = node_type
 		
+		# is the nodes activation function
 		self.activation_function = activation_function
 
 
@@ -25,16 +29,29 @@ class ConnectionGene:
 		enabled = True
 		):
 
+		# specifies the tail of the connection arc 
 		self.out_node_id = out_node_id
+
+		# specifies the head of the connection arc 
 		self.in_node_id = in_node_id
+
+		# wight attribute used to feed forward an input
 		self.weight = weight
+
+		# innovation number used to counter the competing conventions problem 
 		self.innovation_number = innovation_number
+
+		# specifies if the connection is enabled
 		self.enabled = enabled
 
 
 class Genome:
 	def __init__(self, nodes, connections):
+		# list to store all the node genes in
+		# sorted by id -> input nodes are the first nodes, output the last
 		self.nodes = sorted(nodes, key = lambda x: x.node_id)
+
+		# list to store the connection genes in
 		self.connections = connections
 
 
@@ -66,16 +83,11 @@ class Genome:
 		# if the node value has not been calculated yet, calculate it with all the inputs going in
 		# to the node
 		for connection in self.connections:
-			print(connection.__dict__)
-			print(node.node_id)
-			print(connection.out_node_id == node.node_id)
 			if connection.in_node_id == node.node_id and connection.enabled:
 				inputs.append(
 					self.calculate_node(
 						self.nodes[connection.out_node_id]
 					) * connection.weight)
-
-		print(inputs)
 
 		# store the value in the table to 
 		activation = node.activation_function(sum(inputs))
@@ -87,13 +99,14 @@ class Genome:
 		Feeds the given input through the network induced by the genotype
 		'''
 
-
 		# set up a table to store the activations of the different nodes
 		self.table = {}
 
+		# store the input layer activation in the table
 		for index, input_activation in enumerate(list(input_vector)):
 			self.table[index] = input_activation
-
+	
+		# iterate over all output nodes and calculate their activation recursively
 		for node in self.nodes:
 			if node.node_type == 'output':
 				self.calculate_node(node)
@@ -105,14 +118,16 @@ class Genome:
 
 def main():
 	G = Genome(
-		[NodeGene(0, 'input', lambda x: x), NodeGene(1, 'input', lambda x: x), NodeGene(2, 'output', lambda x: x)],
-		[ConnectionGene(0, 2, -0.5, 0), ConnectionGene(1, 2, 0.5, 1)]
+		[NodeGene(0, 'input', lambda x: x), NodeGene(1, 'hidden', lambda x: x), NodeGene(2,
+		'hidden', lambda x: x), NodeGene(3, 'output', lambda x: x)],
+		[ConnectionGene(0, 1, -0.5, 0), ConnectionGene(0, 2, 0.5, 0), ConnectionGene(1, 3, 0.5, 1),
+		ConnectionGene(2, 3, 2, 1)]
 	)
 
 	print([node.__dict__ for node in G.nodes])
 	print([connection.__dict__ for connection in G.connections])
 	
-	print(G.feed_forward([1, 2]))
+	print(G.feed_forward([1]))
 	
 
 if __name__ == '__main__': main()
