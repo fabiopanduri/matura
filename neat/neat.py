346fd9f2c3484412e51e4c3cbd4a88d0d9e33fef
Fabio Panduri <fabio.panduri@hotmail.com>
2022-06-01 18:56:52 +0200

    neat.py: created an extra method to generate a random fully connected population

--- neat/neat.py
+++ neat/neat.py
@@ -17,12 +17,16 @@ class NEAT:
         self,
         population_size,
         nn_base_dimensions,
+        speciation_constants,
+        weight_mutation_constants
     ):
         self.population_size = population_size
         self.nn_dimensions = nn_base_dimensions
         self.global_innovation_number = 0
+        self.speciation_constants = speciation_constants
+        self.weight_mutation_constants = weight_mutation_constants
 
-    def make_population(self):
+    def make_population_empty(self, activation_functions_hidden=[], activation_functions_output=[]):
         """
         Makes a generation of the given population size with the given nn_base_dimensions
         The nets in the population are connection free, so they only contain nodes
@@ -32,10 +36,30 @@ class NEAT:
 
         for i in range(self.population_size):
             self.population.append(
-                Genome.empty_genome(
+                Genome.make_empty_genome(
                     self.nn_dimensions[0],
                     sum(self.nn_dimensions[1:-1]),
                     self.nn_dimensions[-1],
+                    activation_functions_hidden,
+                    activation_functions_output
+                )
+            )
+
+    def make_population_connected(self, activation_functions=[]):
+        """
+        Makes a generation of the given population size with the given nn_base_dimensions
+        The nets in the population are all fully-connected digraphs from the input nodes to
+        the output nodes
+        """
+
+        self.population = []
+
+        for i in range(self.population_size):
+            self.population.append(
+                Genome.make_connected_genome(
+                    self.nn_dimensions[0],
+                    self.nn_dimensions[-1],
+                    activation_functions
                 )
             )
 
@@ -44,12 +68,10 @@ class NEAT:
 
 
 def main():
-    """
-    N = NEAT(1, [4, 3, 2, 1, 4])
-    N.make_population()
+    N = NEAT(1, [4, 3, 2, 1, 4], (1, 1, 1), (0.8, 0.9))
+    N.make_population_connected()
     for p in N.population:
-            p.draw()
-    """
+        p.draw()
 
 
 if __name__ == "__main__":
